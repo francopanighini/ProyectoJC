46a61eae4b30ed60ecff8cf35d4c81fe209b8ef5
fpanighini <fpanighini@itba.edu.ar>
2024-03-04 19:45:53 +0100

    Correctly added acceleration and speed. Added border walls to frame

--- Physics/player.js
+++ Physics/player.js
@@ -1,6 +1,6 @@
 
 const MARIO_STAND_LEFT = 0;
-const MARIO_STAND_RIGHT = 1; 
+const MARIO_STAND_RIGHT = 1;
 const MARIO_WALK_LEFT = 2;
 const MARIO_WALK_RIGHT = 3;
 const MARIO_JUMP_R = 4;
@@ -8,10 +8,9 @@ const MARIO_STOP_JUMP_R = 5;
 const MARIO_JUMP_L = 6;
 const MARIO_STOP_JUMP_L = 7;
 const MARIO_DEAD = 8;
- 
 
-function Player(x, y, map)
-{
+
+function Player(x, y, map) {
 	// Loading spritesheets
 	var mario = new Texture("imgs/mario.png");
 
@@ -52,10 +51,10 @@ function Player(x, y, map)
 
 
 	this.sprite.setAnimation(MARIO_STAND_RIGHT);
-	
+
 	// Set tilemap for collisions
 	this.map = map;
-	
+
 	this.animjump = false;
 	this.direccionjump = "R";
 
@@ -65,273 +64,258 @@ function Player(x, y, map)
 
 	this.m_dead = false;
 	this.c_dead = 0;
-	
+
 	this.speed = 0;
 }
 
-let minWalkSpeed = 30;
-let walkAccel = 30;
-let runAccel = 60;
-let releaseDecel = 180;
-let maxWalkSpeed = 60;
-let maxRunSpeed = 120;
+let minWalkSpeed = 60;
+let walkAccel = 60;
+let runAccel = 120;
+let releaseDecel = 360;
+let maxWalkSpeed = 120;
+let maxRunSpeed = 240;
 
-Player.prototype.moveMario = function(deltaTime)
-{
+Player.prototype.moveMario = function (deltaTime) {
 	let accel = 0;
-	
-	if(keyboard[37] || keyboard[39])
-	{
+
+	if (this.m_dead) {
+		return;
+	}
+
+	if ((keyboard[37] || keyboard[39]) && !(keyboard[37] && keyboard[39])) {
 		// Pressing move buttons
-		if(keyboard[37] && (this.speed > -minWalkSpeed))
+		if (keyboard[37] && (this.speed > -minWalkSpeed))
 			this.speed = -minWalkSpeed;
-		else if(keyboard[39] && (this.speed < minWalkSpeed))
+		else if (keyboard[39] && (this.speed < minWalkSpeed))
 			this.speed = minWalkSpeed;
 		// Prepare acceleration according to action (walk or run)
-		if(keyboard[16])
-		{
-			if(keyboard[37])
+		if (keyboard[16]) {
+			if (keyboard[37])
 				accel = -runAccel;
 			else
 				accel = runAccel;
 		}
-		else
-		{
-			if(keyboard[37])
+		else {
+			if (keyboard[37])
 				accel = -walkAccel;
 			else
 				accel = walkAccel;
 		}
 	}
-	else
-	{
-		if(this.speed > 0)
+	else {
+		if (this.speed > 0)
 			accel = -releaseDecel;
-		else if(this.speed < 0)
+		else if (this.speed < 0)
 			accel = releaseDecel;
 		else
 			accel = 0;
 	}
-	
+
+	//save previous x position
+	let prevX = this.sprite.x;
+
 	// Move according to current speed
 	this.sprite.x = this.sprite.x + this.speed * deltaTime / 1000.0;
 
+	//check for collisions
+	//if speed is positive, use collisionMoveRight, else use collisionMoveLeft
+	if ((this.speed > 0 && this.map.collisionMoveRight(this.sprite))
+		|| (this.speed < 0 && this.map.collisionMoveLeft(this.sprite)))
+		this.sprite.x = prevX;
+
+
 	// Apply acceleration to current speed
-	if(keyboard[37] || keyboard[39])
-	{
+	if ((keyboard[37] || keyboard[39]) && !(keyboard[37] && keyboard[39])) {
 		this.speed = this.speed + accel * deltaTime / 1000.0;
 
 		// Respect maximum speeds
-		if(keyboard[16])
-		{
-			if(Math.abs(this.speed) > maxRunSpeed)
-			{
-				if(this.speed > 0)
+		if (keyboard[16]) {
+			if (Math.abs(this.speed) > maxRunSpeed) {
+				if (this.speed > 0)
 					this.speed = maxRunSpeed;
 				else
 					this.speed = -maxRunSpeed;
 			}
 		}
-		else
-		{
-			if(Math.abs(this.speed) > maxWalkSpeed)
-			{
-				if(this.speed > 0)
+		else {
+			if (Math.abs(this.speed) > maxWalkSpeed) {
+				if (this.speed > 0)
 					this.speed = maxWalkSpeed;
 				else
 					this.speed = -maxWalkSpeed;
 			}
 		}
 	}
-	else
-	{
+	else {
 		// Be careful to stop when current acceleration gets close to zero
-		if(this.speed > 0)
-		{
+		if (this.speed > 0) {
 			this.speed = this.speed + accel * deltaTime / 1000.0;
-			if(this.speed < minWalkSpeed)
+			if (this.speed < minWalkSpeed)
 				this.speed = 0;
 		}
-		else if(this.speed < 0)
-		{
+		else if (this.speed < 0) {
 			this.speed = this.speed + accel * deltaTime / 1000.0;
-			if(this.speed > -minWalkSpeed)
+			if (this.speed > -minWalkSpeed)
 				this.speed = 0;
 		}
+	}
+
+	// Set animation according to current speed
+	if (this.speed > 0) {
+		if (!this.animjump) {
+			if (this.sprite.currentAnimation != MARIO_WALK_RIGHT)
+				this.sprite.setAnimation(MARIO_WALK_RIGHT);
+			// if(this.speed > 60){
+			// 	this.sprite.setAnimation(MARIO_RUN_RIGHT);
+			// }
+		} else {
+			if (this.sprite.currentAnimation != MARIO_JUMP_R)
+				this.sprite.setAnimation(MARIO_JUMP_R);
+		}
+		this.direccionjump = "R";
+	} else if (this.speed < 0) {
+		if (!this.animjump) {
+			if (this.sprite.currentAnimation != MARIO_WALK_LEFT)
+				this.sprite.setAnimation(MARIO_WALK_LEFT);
+			// if(this.speed < -60){
+			// 	this.sprite.setAnimation(MARIO_RUN_LEFT);
+			// }
+		} else {
+			if (this.sprite.currentAnimation != MARIO_JUMP_L)
+				this.sprite.setAnimation(MARIO_JUMP_L);
+		}
+		this.direccionjump = "L";
+	} else {
+		if (this.sprite.currentAnimation == MARIO_WALK_LEFT)
+			this.sprite.setAnimation(MARIO_STAND_LEFT);
+		if (this.sprite.currentAnimation == MARIO_WALK_RIGHT)
+			this.sprite.setAnimation(MARIO_STAND_RIGHT);
+	}
+	// TODO add double animation for running
+
+	// Check arrow up key. If pressed, jump.
+	if (this.speed < 0 && keyboard[38]) {
+		if (this.sprite.currentAnimation != MARIO_JUMP_L)
+			this.sprite.setAnimation(MARIO_JUMP_L);
+
+		this.animjump = true;
+		this.direccionjump = "L";
+
+	} else if (this.speed > 0 && keyboard[38]) // KEY_RIGHT
+	{
+		if (this.sprite.currentAnimation != MARIO_JUMP_R)
+			this.sprite.setAnimation(MARIO_JUMP_R);
+
+		this.animjump = true;
+		this.direccionjump = "R";
+	} else if (keyboard[38]) // KEY_UP
+	{
+		if (this.direccionjump == "L") {
+			if (this.sprite.currentAnimation != MARIO_JUMP_L)
+				this.sprite.setAnimation(MARIO_JUMP_L);
+		} else {
+			if (this.sprite.currentAnimation != MARIO_JUMP_R)
+				this.sprite.setAnimation(MARIO_JUMP_R);
+		}
+
+		this.animjump = true;
+	}
+
+	if (this.sprite.x < -2) {
+		this.sprite.x = 0;
+	} else if (this.sprite.x > 480) {
+		this.sprite.x = 480;
 	}
 
 	console.log("Speed = " + this.speed);
 	//console.log("Accel = " + accel);
 }
 
-Player.prototype.update = function(deltaTime)
-{
+Player.prototype.update = function (deltaTime) {
 
-	if(this.m_dead){
-		if(this.sprite.currentAnimation != MARIO_DEAD)
-			this.sprite.setAnimation(MARIO_DEAD);		
-		if (this.c_dead < 5){
+	if (this.m_dead) {
+		if (this.sprite.currentAnimation != MARIO_DEAD)
+			this.sprite.setAnimation(MARIO_DEAD);
+		if (this.c_dead < 5) {
 			this.sprite.y -= 16;
-		}else if(this.c_dead > 80 && this.c_dead < 100 ){
+		} else if (this.c_dead > 80 && this.c_dead < 100) {
 			this.sprite.y += 2;
 		}
 
 		this.c_dead = this.c_dead + 1;
 	}
-	else if(keyboard[37] && keyboard[38]) // KEY_LEFT
-	{
-		if(this.sprite.currentAnimation != MARIO_JUMP_L)
-			this.sprite.setAnimation(MARIO_JUMP_L);
-		this.sprite.x -= 2;
-		if(this.map.collisionMoveLeft(this.sprite))
-			this.sprite.x += 2;
-		
-		this.animjump = true;
-		this.direccionjump = "L";
-	
-	}else if(keyboard[39] && keyboard[38]) // KEY_RIGHT
-	{
-		if(this.sprite.currentAnimation != MARIO_JUMP_R)
-			this.sprite.setAnimation(MARIO_JUMP_R);
-		this.sprite.x += 2;
-		if(this.map.collisionMoveLeft(this.sprite))
-			this.sprite.x -= 2;
-		
-			
-	
-		this.animjump = true;
-		this.direccionjump ="R";
-	
-	}
-	else if(keyboard[37]) // KEY_LEFT
-	{
-		if(this.sprite.currentAnimation != MARIO_WALK_LEFT)
-			this.sprite.setAnimation(MARIO_WALK_LEFT);
-		this.sprite.x -= 2;
-		if(this.map.collisionMoveLeft(this.sprite))
-			this.sprite.x += 2;
-		this.direccionjump = "L";
-	}
-	else if(keyboard[39]) // KEY_RIGHT
-	{
-		if(this.sprite.currentAnimation != MARIO_WALK_RIGHT)
-			this.sprite.setAnimation(MARIO_WALK_RIGHT);
-		this.sprite.x += 2;
-		if(this.map.collisionMoveRight(this.sprite)){
-			this.sprite.x -= 2;
-		}
-			
-		this.direccionjump = "R";
-	}
-	else if(keyboard[38]) // KEY_UP
-	{
-		if(this.direccionjump == "L"){
-			if(this.sprite.currentAnimation != MARIO_JUMP_L)
-				this.sprite.setAnimation(MARIO_JUMP_L);
-		}else{
-			if(this.sprite.currentAnimation != MARIO_JUMP_R)
-				this.sprite.setAnimation(MARIO_JUMP_R);
-		}
-		
-		this.animjump = true;
-		//this.direccionjump ="L";
-		
-	}
-	else 
-	{
-		if(this.sprite.currentAnimation == MARIO_WALK_LEFT)
-			this.sprite.setAnimation(MARIO_STAND_LEFT);
-		if(this.sprite.currentAnimation == MARIO_WALK_RIGHT)
-			this.sprite.setAnimation(MARIO_STAND_RIGHT);
-	}
 
-
-	if (this.sprite.x < -2){
-		this.sprite.x += 2;
-	}else if(this.sprite.x > 480){
-		this.sprite.x -= 2;
-	}
-
-	if(this.bJumping)
-	{
+	if (this.bJumping) {
 
 		this.jumpAngle += 4;
-		if(this.jumpAngle == 180)
-		{
+		if (this.jumpAngle == 180) {
 			this.bJumping = false;
 			this.sprite.y = this.startY;
-			if(this.animjump){
+			if (this.animjump) {
 
-				
-				if (this.direccionjump == "R"){
-					if(this.sprite.currentAnimation != MARIO_STOP_JUMP_R)
-					this.sprite.setAnimation(MARIO_STOP_JUMP_R);
-				}else{
-					if(this.sprite.currentAnimation != MARIO_STOP_JUMP_L)
-					this.sprite.setAnimation(MARIO_STOP_JUMP_L);
+
+				if (this.direccionjump == "R") {
+					if (this.sprite.currentAnimation != MARIO_STOP_JUMP_R)
+						this.sprite.setAnimation(MARIO_STOP_JUMP_R);
+				} else {
+					if (this.sprite.currentAnimation != MARIO_STOP_JUMP_L)
+						this.sprite.setAnimation(MARIO_STOP_JUMP_L);
 				}
-				
+
 				this.animjump = false;
 			}
 		}
-		else
-		{
-			
+		else {
+
 			this.sprite.y = this.startY - 96 * Math.sin(3.14159 * this.jumpAngle / 180);
-			if(this.jumpAngle > 90){
+			if (this.jumpAngle > 90) {
 				this.bJumping = !this.map.collisionMoveDown(this.sprite);
 			}
 
 		}
 	}
-	else
-	{
+	else {
 		// Move Bub so that it is affected by gravity
 		this.sprite.y += 4;
-		if(!this.m_dead && this.map.collisionMoveDown(this.sprite))
-		{
-			
+		if (!this.m_dead && this.map.collisionMoveDown(this.sprite)) {
+
 			//this.sprite.y -= 2;
 
 			// Check arrow up key. If pressed, jump.
-			if(keyboard[38] && !this.m_dead)
-			{
+			if (keyboard[38] && !this.m_dead) {
 				this.bJumping = true;
 				this.jumpAngle = 0;
 				this.startY = this.sprite.y;
-			}else if(this.animjump){
-				if (this.direccionjump == "R"){
-					if(this.sprite.currentAnimation != MARIO_STOP_JUMP_R)
-					this.sprite.setAnimation(MARIO_STOP_JUMP_R);
-				}else{
-					if(this.sprite.currentAnimation != MARIO_STOP_JUMP_L)
-					this.sprite.setAnimation(MARIO_STOP_JUMP_L);
+			} else if (this.animjump) {
+				if (this.direccionjump == "R") {
+					if (this.sprite.currentAnimation != MARIO_STOP_JUMP_R)
+						this.sprite.setAnimation(MARIO_STOP_JUMP_R);
+				} else {
+					if (this.sprite.currentAnimation != MARIO_STOP_JUMP_L)
+						this.sprite.setAnimation(MARIO_STOP_JUMP_L);
 				}
-				
+
 				this.animjump = false;
 			}
 		}
-		
+
 	}
-	
+
 	// Update sprites
 	this.sprite.update(deltaTime);
 }
 
-Player.prototype.draw = function()
-{
+Player.prototype.draw = function () {
 	this.sprite.draw();
 }
 
-Player.prototype.collisionBox = function()
-{
+Player.prototype.collisionBox = function () {
 	var box = new Box(this.sprite.x + 2, this.sprite.y, this.sprite.x + this.sprite.width - 4, this.sprite.y + this.sprite.height);
-	
+
 	return box;
 }
 
-Player.prototype.dead = function()
-{
+Player.prototype.dead = function () {
 	this.m_dead = true;
 }
 
