23eb1ed1e6f0bc76292d7df5021bff4a19be28fe
fpanighini <fpanighini@itba.edu.ar>
2024-03-18 21:14:21 +0100

    Improved start screen

--- Physics/scene_start.js
+++ Physics/scene_start.js
@@ -8,7 +8,7 @@ function SceneStart()
 	var img = new Texture("imgs/start.png");
 	var img2 = new Texture("imgs/varied.png");
 	
-	
+	this.coin = new Coin(165, 50);
 
 	this.selec = [70+140,105+140,130+150];
 	this.i = 0;
@@ -40,10 +40,12 @@ SceneStart.prototype.update = function(deltaTime)
 		this.setaQuad.y = this.selec[this.i];
 	}
 
-
-
 	// Keep track of time
 	this.currentTime += deltaTime;
+
+	// Update entities
+	this.coin.update(deltaTime);
+	// this.question_box.update(deltaTime);
 }
 
 SceneStart.prototype.draw = function ()
@@ -56,8 +58,12 @@ SceneStart.prototype.draw = function ()
 	context.fillStyle = "rgb(199, 253, 255)";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
+	// Draw entities
+
 	context.save();
 	this.start.draw();
+	this.coin.draw();
+	// this.question_box.draw();
 	context.restore();
 
 	
@@ -69,14 +75,47 @@ SceneStart.prototype.draw = function ()
 
 	// Draw text
 	var text = "TIME";
-	context.font = "24px mario";
+	context.font = "18px mario";
 	var textSize = context.measureText(text);
 	context.fillStyle = "White";
-	context.fillText(text, 512 - textSize.width - 16, textSize.width/2);
+	// right side of the screen
+	context.fillText(text, 400, 48);
 	
-	text = Math.floor(this.currentTime / 1000);
+	// text = Math.floor(400 - this.currentTime / 1000);
+	// var textSize = context.measureText(text);
+	// if(textSize.width < 20)
+	// 	context.fillText(text, 454, 48 + 24);
+	// else if(textSize.width < 40)
+	// 	context.fillText(text, 436, 48 + 24);
+	// else
+	// 	context.fillText(text, 418, 48 + 24);
+
+	// Draw text
+	var text = "WORLD";
 	var textSize = context.measureText(text);
-	context.fillText(text, 512 - textSize.width - 16, textSize.width/2 + 80);
+	context.fillStyle = "White";
+	context.fillText(text, 286, 48);
+
+	text = "1-1";
+	var textSize = context.measureText(text);
+	context.fillText(text, 286, 48 + 24);
+
+	// Draw text
+	var text = "x00";
+	var textSize = context.measureText(text);
+	context.fillStyle = "White";
+	context.fillText(text, 190, 48+24);
+
+	// Draw text
+	var text = "MARIO";
+	var textSize = context.measureText(text);
+	context.fillStyle = "White";
+	context.fillText(text, 32, 48);
+
+	text = "000000";
+	var textSize = context.measureText(text);
+	context.fillText(text, 32, 48 + 24);
+
 
 
 	if(keyboard[32])
